133b22216537029c37d2f9ac7b128560097c3bb9
yuval almaliah <yuv.almaliah@hmail.com>
2018-12-02 01:41:39 +0200

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 import {ServerComponent} from './server/server.component';
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { ErrorMessageComponent } from './error-message/error-message.component';
+import { WarningMessageComponent } from './warning-message/warning-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    ErrorMessageComponent,
+    WarningMessageComponent
   ],
   imports: [
     BrowserModule
