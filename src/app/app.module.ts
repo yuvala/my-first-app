912bfd9b9efb7df6c21cea72e583a4d3709d7596
yuval almaliah <yuv.almaliah@hmail.com>
2018-12-04 22:50:23 +0200

    ddd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {ServerComponent} from './server/server.component';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 import { ErrorMessageComponent } from './error-message/error-message.component';
 import { WarningMessageComponent } from './warning-message/warning-message.component';
+import { UserNameSectionComponent } from './user-name-section/user-name-section.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,12 @@ import { WarningMessageComponent } from './warning-message/warning-message.compo
     ServerComponent,
     ServersComponent,
     ErrorMessageComponent,
-    WarningMessageComponent
+    WarningMessageComponent,
+    UserNameSectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,19 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { SSL_OP_SINGLE_DH_USE } from 'constants';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .serverFrame {
+                   border:1px red solid;
+                   margin:10px;
+                   padding:10px;
+            }
+            .btn-section {
+                display:flex;
+            }
+            .btn-section div {
+                margin:10px;
+            }
+    `]
 })
 
 
 export class ServerComponent implements OnInit {
     allowButtonClick = false;
+    serverCreationStatus = 'No server was created!';
+    serverName = 'walla';
     constructor() {
         setTimeout(() => {
             this.allowButtonClick = true;
         }, 5000);
     }
-    ngOnInit() {
+    ngOnInit () {
 
+    }
+    onServerCreated () {
+        this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    }
+    onServerNameUpdated (event: Event) {
+        this.serverName = (<HTMLInputElement>event.target).value;
+        console.log(event);
     }
 }
